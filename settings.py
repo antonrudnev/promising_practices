fab183abcb7ee338090fc797b5b6e2ce4cbdc289
Anton Rudnev <anton.rudnev@gmail.com>
2018-06-21 23:22:35 -0400

    "Create" use case was implemented. Refactoring.

--- settings.py
+++ settings.py
@@ -2,6 +2,17 @@ SOLR = "http://54.173.176.161:8983/solr/promising_practices2"
 ITEMS_PER_PAGE = 15
 PAGER_RANGE = 2
 
-STATES = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY",
-          "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND",
+STATES = ["RI", "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS",
+          "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND",
           "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+
+INTERVENTION_GOALS = ["Harm Reduction", "Prevention", "Recovery", "Treatment"]
+
+IMPLEMENTERS = ["Community", "County", "Faith_Communities", "Health Care", "National",  "Police", "Prisons/Jails",
+                "Schools", "State"]
+
+PROGRAM_COMPONENTS = ["Behavioral", "Communication", "Lock_In", "MAT", "Mental_Health", "Monitoring", "Naloxone",
+                      "Opioid_Disposal", "Opioid_Storage", "PDMP", "Policy", "Recovery_Program", "Remote",
+                      "Supervised_Consumption_Sites", "Syringe_Services", "Training"]
+
+POPULATIONS = ["Adults", "General", "Neonates", "Pregnant_Women", "Youth"]
