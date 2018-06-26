358cfe30f03cddbae2c8029ca556086c5c97982b
Anton Rudnev <arudnev@air.org>
2018-06-26 12:12:37 -0400

    Authentication implemented.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from db import close_db
 from flask import Flask
-import practice
+import auth, practice
 
 app = Flask(__name__)
+app.register_blueprint(auth.bp)
 app.register_blueprint(practice.bp)
 app.add_url_rule("/", endpoint="practice", view_func=practice.index)
 app.config.from_mapping(SECRET_KEY="dev")
