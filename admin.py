5c6eeb70e291598351736b6b3ea9d867bba9703f
Anton Rudnev <arudnev@air.org>
2018-06-28 13:22:18 -0400

    Bug fixes.

--- admin.py
+++ admin.py
@@ -18,7 +18,7 @@ def users():
     users_tbl = get_db().execute("SELECT T.user_name, "
                                  "T.full_name, "
                                  "T.enabled, "
-                                 "GROUP_CONCAT(role_name) AS roles "
+                                 "IFNULL(GROUP_CONCAT(role_name), '') AS roles "
                                  "FROM (SELECT user_name, "
                                  "full_name, "
                                  "enabled, "
@@ -31,7 +31,7 @@ def users():
     users = [{"user_name": user["user_name"],
               "full_name": user["full_name"],
               "enabled": user["enabled"],
-              "roles": user["roles"].split(",") if user["roles"] else []} for user in users_tbl]
+              "roles": user["roles"].split(",")} for user in users_tbl]
 
     return render_template("admin/users.html", users=users)
 
@@ -40,19 +40,16 @@ def users():
 @login_required
 @permission_required("SECURITY_ADMIN")
 def roles():
-    roles_tbl = get_db().execute("WITH T AS ("
-                                 "SELECT role_name,"
+    roles_tbl = get_db().execute("SELECT T.role_name, "
+                                 "IFNULL(GROUP_CONCAT(permission_name), '') AS permissions, "
+                                 "IFNULL(GROUP_CONCAT(enabled), '') AS enabled_list "
+                                 "FROM (SELECT role_name,"
                                  "permission_name,"
                                  "CASE WHEN role_id ISNULL THEN 0 ELSE 1 END AS enabled "
                                  "FROM role CROSS JOIN permission "
                                  "LEFT JOIN role_permission ON role.id = role_permission.role_id "
                                  "AND permission.id = role_permission.permission_id "
-                                 "ORDER BY role_name, permission_name) "
-
-                                 "SELECT role_name, "
-                                 "GROUP_CONCAT(permission_name) AS permissions, "
-                                 "GROUP_CONCAT(enabled) AS enabled_list "
-                                 "FROM T GROUP BY role_name").fetchall()
+                                 "ORDER BY role_name, permission_name) AS T GROUP BY role_name").fetchall()
 
     roles = [{"role_name": role["role_name"],
               "permissions": role["permissions"].split(","),
